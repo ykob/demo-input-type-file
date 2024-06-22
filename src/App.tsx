20f3ee9db0a3b70609e8ac57ae8cfaa49e4fa716
ykob <info@tplh.net>
2024-06-22 21:32:51 +0900

    added fileReader and onChange event for input element.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 function App() {
-  return <div></div>;
+  const fileReader = new FileReader();
+
+  fileReader.onload = () => {
+    console.log(fileReader.result);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      return;
+    }
+    if (file.type !== "image/png" && file.type !== "image/jpeg") {
+      console.log("file type is not png or jpeg.");
+    }
+    fileReader.readAsDataURL(file);
+  };
+
+  return (
+    <div>
+      <h1>Demo input[type="file"]</h1>
+      <input type="file" accept="image/png, image/jpeg" onChange={onChange} />
+    </div>
+  );
 }
 
 export default App;
